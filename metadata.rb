57bd4884602918df20b6414483f659e49b0a5561
Jay Flowers <jay.flowers@gmail.com>
2018-01-16 10:09:29 -0500

    commit for version : 1.0.3

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jay.flowers@gmail.com"
 license          "Apache 2.0"
 description      "ec2 resources"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.2"
+version          "1.0.3"
 
 depends "aws", '> 7.2'
 depends "format"
